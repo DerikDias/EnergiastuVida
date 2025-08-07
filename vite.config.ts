179c6fe047ae41bd4c90da334a940a322184ff17
DerikDias <130597423+DerikDias@users.noreply.github.com>
2025-08-07 13:08:41 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/EnergiasTuVida/',
+  base: '/EnergiastuVida/',
   plugins: [react()],
 });
